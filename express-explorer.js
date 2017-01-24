da6477577faad5a35fa2db3bd33acc3fa0e73878
Giovanni Bruno <giovanni.bruno@soluzionifutura.it>
2017-01-24 02:16:14 +0100

    Express Router enriched

--- express-explorer.js
+++ express-explorer.js
@@ -1,38 +1,72 @@
 'use strict';
+const flatten = require('array-flatten');
+const slice = Array.prototype.slice;
+const debug = require('debug')('express:router');
+const Layer = require('express/lib/router/layer');
+const Router = require('express/lib/router');
 
-module.exports = (express) => {
-  const appUseFn = express.application.use;
-  //console.log(express.application.use.toString());
-  express.application.use = (...args) => {
-    console.log('use on router');
-    return appUseFn.apply(express, arguments);  
-  };
-  return explorerMiddleware;
-}
+const routes = {};
 
-function explorerMiddleware(req, res) {
-  const routes = {}; 
-  dig(req.app._router.stack, routes);
-  //console.log(req.app._router.stack);  
-  //console.log(routes);
-  res.send('explorer');
-}
+Router.use = function use(fn) {
+  let offset = 0;
+  let path = '/';
 
-function dig(stack, routes){
-  const l = stack.length;
-  for (let i = 0; i < l; i++) {
-    const layer = stack[i];
-    const route = layer.route;
+  if (typeof fn !== 'function') {
+    let arg = fn;
 
-    if (route){
-      const path = route.path; 
-      const methods = route.methods;
-    
-      if (!routes[path]) routes[path] = { methods: methods };
-      else Object.assign(routes[path].methods, methods);
-    } else if (layer.name === 'router') {
-      //console.log(layer.handle);
-      dig(layer.handle.stack, routes);
+    while (Array.isArray(arg) && arg.length !== 0) arg = arg[0];
+
+    if (typeof arg !== 'function') {
+      offset = 1;
+      path = fn;
     }
   }
+
+  const callbacks = flatten(slice.call(arguments, offset));
+  if (callbacks.length === 0) throw new TypeError('Router.use() requires middleware functions');
+
+  const l = callbacks.length;
+  for (let i = 0; i < l; i++) {
+    const fn = callbacks[i];
+
+    if (typeof fn !== 'function') throw new TypeError('Router.use() requires middleware function but got a ' + gettype(fn));
+
+    debug('use %s %s', path, fn.name || '<anonymous>');
+
+    if (fn.name === 'router') dig(fn.stack, path);
+    //todo questo non va mica qui. questo andrà messo negli altri metodi del router.
+    //dig(this.stack);
+
+    const layer = new Layer(path, {
+      sensitive: this.caseSensitive,
+      strict: false,
+      end: false
+    }, fn);
+
+    layer.route = undefined;
+
+    this.stack.push(layer);
+  }
+
+  return this;
+};
+
+
+module.exports = (req, res) => {
+  console.log(routes);
+  res.send('explorer');
+};
+
+function dig(stack, prefix) {
+  prefix = prefix || '';
+  stack.forEach(layer => {
+    const route = layer.route;
+
+    if (route) {
+      const path = prefix + route.path;
+      const methods = route.methods;
+
+      routes[path] = Object.assign({}, routes[path], methods);
+    }
+  });
 }
--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 'use strict';
 const express = require('express');
-const explorer = require('./express-explorer')(express);
+const explorer = require('./express-explorer');
 const app = new express();
 const router = express.Router();
 const http = require('http');
 
 app.use('/explorer', explorer);
+
+const getParams = (path) => {
+    return [ path, (req, res) => { res.send('ok! ' + path)} ];
+};
 
 [
   '/home',
@@ -15,20 +19,27 @@ app.use('/explorer', explorer);
  app.get(route, (req, res) => {
    res.send(route);
  });
- 
+
  app.post(route, (req, res) => {
    res.send(route + 'post');
  });
 
- router.get(route + 'r', (req, res) => {
-   res.send(route + 'r');
+ router.get(route + 'rrrr', (req, res) => {
+   res.send(route + 'rrrr');
  });
+
+ router.post(route + 'rrrr', (req, res) => {
+    res.send(route + 'rrrr');
+  });
 });
 
 app.use('/rout', router);
 
+/*app.get(...getParams('/appget'));
+router.get(...getParams('/routerget'));
+app.use('/rirrorarro', router);*/
+
 app.listen(8080, () => {
   console.log('Listening on port 8080');
-  console.log('Requiring /explorer');  
   http.get('http://localhost:8080/explorer');
 });
